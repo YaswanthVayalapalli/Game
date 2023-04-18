04fbb3f895de17f76c0dc3c3dd69a7b37e63e66a
YaswanthVayalapalli <129929358+YaswanthVayalapalli@users.noreply.github.com>
2023-04-18 11:22:09 -0400

    Update Movement_monopoly.java

--- Movement_monopoly.java
+++ Movement_monopoly.java
@@ -1,32 +1,50 @@
 //To retrieve data from database about position of the player and move checker and reinsert new position of the player into database
 public class Movement_monopoly {
-	int Row = 0, Col = 0;
+	int Row = 0;
+	int Col = 10;
 	Movement_monopoly(int steps) {
-		if (Row <= 8 && Col == 0 && Row!=0) {
-			Row = Row - steps;
-		} else {
-			if(Row==8) {
-				Col=Col-steps;
+		if(Row==0 && Col>0) {
+			if(steps>Col) {
+				Row=Row-Col+steps;
 			}
-			else {
-				Col = Col + steps;
-				if (Row >= 0 && Col > 8) {
-					while (Col > 8) {
-						Row++;
-						Col--;
-					}
-				}
-				if (Row >= 8 && Col >= 8) {
-					while(Col>8) {
-						Col--;
-						Row++;
-					}
-					while (Row > 8) {
-						Col--;
-						Row--;
-					}
-				}
-			}	
+			while(steps!=0) {
+				Col--;
+				steps--;
+			}
 		}
+		if(Col==0 && Row<10) {
+			if(steps>10-Row) {
+				Col=Col-10+Row+steps;
+				steps=steps-Col;
+			}
+			while(steps!=0) {
+				Row++;
+				steps--;
+			}
+		}
+		if(Row==10 && Col<10) {
+			if(steps>10-Col) {
+				Row=Col+steps-10+Row;
+				steps=steps-Row;
+			}
+			while(steps!=0) {
+				Col++;
+				steps--;
+			}
+		}
+		if(Col==10 && Row>0) {
+			if(steps>Row) {
+				Col=Col-Row;
+				steps=steps+10-Row;
+			}
+			while(steps!=0) {
+				Row--;
+				steps--;
+			}
+		}
+		rules(Row, Col);
+	}
+	void rules(int Row, int Col) {
+		new Monopoly_rules(Row, Col);
 	}
 }
