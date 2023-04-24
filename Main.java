a037a133e9c75126fffa2483fdf17919b26ad6cd
YaswanthVayalapalli <129929358+YaswanthVayalapalli@users.noreply.github.com>
2023-04-24 14:00:17 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
-public class Main {
-	public static void main(String[] args) {
-		Dice Dice_obj= new Dice();
-		int steps = Dice_obj.roll();
-		Movement_monopoly mov_mon_obj = new Movement_monopoly(steps);
-	}
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OodApplication.class, args);
+    }
 }
